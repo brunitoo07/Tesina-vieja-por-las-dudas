d4009a36a5051aa5d9e6391f8d80c873d3b9948d
brunitoo07 <brunocameille@alumnos.itr3.edu.ar>
2025-03-27 20:12:17 -0300

    supuestamente anda vista y botones desde compra y login

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://192.168.2.176/CODEIGNITERNUEVO/public';
+    public string $baseURL = 'http://localhost/Tesina/public';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
