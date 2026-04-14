a1e33e0d65d6186a47fbab0d777ae720dfb4844a
Bruno Cameille <balexc777@gmail.com>
2026-04-14 17:45:16 -0600

    actualizar titulo
    asistente virutalk nombre nuevo

--- app/Views/chat_profesional.php
+++ app/Views/chat_profesional.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Asistente Virtual Profesional - EcoVolt</title>
+    <title>Asistente Virtual Profesional sisi - EcoVolt</title>
     <style>
         .chat-container {
             position: fixed;
